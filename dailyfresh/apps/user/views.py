b6d5bd1bf168894a655c41498dbc5038e726a254
chenqiang <chenqiang97@foxmail.com>
2018-11-11 18:18:01 +0800

    注册用户激活,celery发送激活邮件

--- dailyfresh/apps/user/views.py
+++ dailyfresh/apps/user/views.py
@@ -1,15 +1,63 @@
 import re
 
+from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
+from django.http import HttpResponse
 from django.shortcuts import render,redirect
-
+# 类视图
+from django.views.generic import View
 # Create your views here.
 from user.models import User
+# 用于数据加密,限时
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, SignatureExpired
+from django.conf import settings
+from celery_tasks.tasks import send_register_active_email
 
 
 def register(request):
-    '''显示注册页面'''
-    return render(request,'register.html')
+    if request.method == 'GET':
+        '''显示注册页面'''
+        return render(request,'register.html')
+    else:
+        '''进行注册处理'''
+        # 接收数据
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+
+        # 进行数据校验
+        if not all([username, password, email]):
+            # 数据不完整
+            return render(request, 'register.html', {'errmsg': '数据不完整'})
+
+        # 校验邮箱
+        if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+            return render(request, 'register.html', {'errmsg': '邮箱格式不正确'})
+
+        if allow != 'on':
+            return render(request, 'register.html', {'errmsg': '请同意协议'})
+
+        # 校验用户名是否重复
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            # 用户名不存在
+            user = None
+
+        if user:
+            # 用户名已存在
+            return render(request, 'register.html', {'errmsg': '用户名已存在'})
+
+        # 进行业务处理: 进行用户注册
+        # 内置系统,创建用户
+        user = User.objects.create_user(username, email, password)
+        user.is_active = 0
+        user.save()
+
+        # 返回应答, 跳转到首页
+        # reverse反向解析函数
+        return redirect(reverse('goods:index'))
 
 def register_handle(request):
     '''进行注册处理'''
@@ -51,3 +99,145 @@ def register_handle(request):
     # 返回应答, 跳转到首页
     # reverse反向解析函数
     return redirect(reverse('goods:index'))
+
+# /user/register
+# 常用方法: GET POST PUT DELETE OPTION
+class RegisterView(View):
+    '''注册'''
+    def get(self, request):
+        '''显示注册页面'''
+        return render(request, 'register.html')
+
+    def post(self, request):
+        '''进行注册处理'''
+        # 接收数据
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+
+        # 进行数据校验
+        if not all([username, password, email]):
+            # 数据不完整
+            return render(request, 'register.html', {'errmsg': '数据不完整'})
+
+        # 校验邮箱
+        if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+            return render(request, 'register.html', {'errmsg': '邮箱格式不正确'})
+
+        if allow != 'on':
+            return render(request, 'register.html', {'errmsg': '请同意协议'})
+
+        # 校验用户名是否重复
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            # 用户名不存在
+            user = None
+
+        if user:
+            # 用户名已存在
+            return render(request, 'register.html', {'errmsg': '用户名已存在'})
+
+        # 进行业务处理: 进行用户注册
+        user = User.objects.create_user(username, email, password)
+        user.is_active = 0
+        user.save()
+
+        # 发送激活邮件，包含激活链接: http://127.0.0.1:8000/user/active/---(id号)
+        # 激活链接中需要包含用户的身份信息, 并且要把身份信息进行加密
+
+        # 加密用户的身份信息，生成激活token,限时1小时
+        # 密钥此处使用setting中的SECRET_KEY
+        serializer = Serializer(settings.SECRET_KEY, 3600)
+        # 加密信息
+        info = {'confirm':user.id}
+        # 加密
+        token = serializer.dumps(info) # bytes
+        token = token.decode()
+
+        # 发邮件 较费时,,,celery异步发送
+        # celery_tasks.tasks
+        send_register_active_email.delay(email, username, token)
+
+        # 返回应答, 跳转到首页
+        return redirect(reverse('goods:index'))
+
+class ActiveView(View):
+    '''用户激活'''
+    def get(self, request, token):
+        '''进行用户激活'''
+        # 进行解密，获取要激活的用户信息
+        serializer = Serializer(settings.SECRET_KEY, 3600)
+        try:
+            # 解密
+            info = serializer.loads(token)
+            # 获取待激活用户的id
+            user_id = info['confirm']
+
+            # 根据id获取用户信息
+            user = User.objects.get(id=user_id)
+            user.is_active = 1
+            user.save()
+
+            # 跳转到登录页面
+            return redirect(reverse('user:login'))
+        except SignatureExpired as e:
+            # 激活链接已过期
+            return HttpResponse('激活链接已过期')
+
+# /user/login
+class LoginView(View):
+    '''登录'''
+    def get(self, request):
+        '''显示登录页面'''
+        # # 判断是否记住了用户名
+        # if 'username' in request.COOKIES:
+        #     username = request.COOKIES.get('username')
+        #     checked = 'checked'
+        # else:
+        #     username = ''
+        #     checked = ''
+
+        # 使用模板
+        return render(request, 'login.html', {'username':username, 'checked':checked})
+
+    # def post(self, request):
+    #     '''登录校验'''
+    #     # 接收数据
+    #     username = request.POST.get('username')
+    #     password = request.POST.get('pwd')
+    #
+    #     # 校验数据
+    #     if not all([username, password]):
+    #         return render(request, 'login.html', {'errmsg':'数据不完整'})
+    #
+    #     # 业务处理:登录校验
+    #     user = authenticate(username=username, password=password)
+    #     if user is not None:
+    #         # 用户名密码正确
+    #         if user.is_active:
+    #             # 用户已激活
+    #             # 记录用户的登录状态
+    #             login(request, user)
+    #
+    #             # 跳转到首页
+    #             response = redirect(reverse('goods:index')) # HttpResponseRedirect
+    #
+    #             # 判断是否需要记住用户名
+    #             remember = request.POST.get('remember')
+    #
+    #             if remember == 'on':
+    #                 # 记住用户名
+    #                 response.set_cookie('username', username, max_age=7*24*3600)
+    #             else:
+    #                 response.delete_cookie('username')
+    #
+    #             # 返回response
+    #             return response
+    #         else:
+    #             # 用户未激活
+    #             return render(request, 'login.html', {'errmsg':'账户未激活'})
+    #     else:
+    #         # 用户名或密码错误
+    #         return render(request, 'login.html', {'errmsg':'用户名或密码错误'})
--- dailyfresh/dailyfresh/settings.py
+++ dailyfresh/dailyfresh/settings.py
@@ -126,3 +126,14 @@ TINYMCE_DEFAULT_CONFIG = {
     'width': 600,
     'height': 400,
 }
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.qq.com'
+EMAIL_PORT = 25
+#发送邮件的邮箱
+EMAIL_HOST_USER = 'chenqiang97@foxmail.com'
+#在邮箱中设置的客户端授权密码
+EMAIL_HOST_PASSWORD = 'ifhxottlshddeacd'
+#收件人看到的发件人
+EMAIL_FROM = '天天生鲜<chenqiang97@foxmail.com>'
